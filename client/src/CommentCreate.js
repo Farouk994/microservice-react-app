20a53edf7cb84a92bfbc345393bedc6d5fcb0785
Farouk <faroukkisuule55@gmail.com>
2021-06-19 10:39:10 -0400

    CommentCreate has been created

---
+++ client/src/CommentCreate.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const CommentCreate = ({ postId }) => {
+  const [content, setContent] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await axios.post(`http://localhost:3001/posts/${postId}/comments`, {
+      content,
+    });
+    setContent('');
+    console.log(content);
+  };
+  return (
+    <div>
+      <form className='form-group' onSubmit={onSubmit}>
+        <div>
+          <label>
+            <h5>New Comment</h5>
+          </label>
+          <input
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            className='form-control'
+          ></input>
+        </div>
+        <button className='btn btn-primary'>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default CommentCreate;
